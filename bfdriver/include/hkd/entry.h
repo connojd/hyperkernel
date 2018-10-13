e9f9a9e177e9051694574d5655af3b7d328ab783
Connor Davis <davisc@ainfosec.com>
2018-10-12 22:35:53 -0600

    Begin driver interface
    Signed-off-by: Connor Davis <davisc@ainfosec.com>

---
+++ bfdriver/include/hkd/entry.h
@@ -0,0 +1,39 @@
+/*
+ * Bareflank PV Interface
+ * Copyright (C) 2018 Assured Information Security, Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#define HKD_NAME "hkd"
+
+/* -------------------------------------------------------------------------- */
+/* IOCTL Interface                                                            */
+/* -------------------------------------------------------------------------- */
+
+#define HKD_SET_SIGNAL_CMD 1
+#define HKD_SET_SIGNAL_WATCHER_CMD 2
+
+#ifdef __linux__
+
+#include <linux/pid.h>
+
+#define HKD_MAJOR 151
+#define HKD_SET_SIGNAL \
+       _IOW(HKD_MAJOR, HKD_SET_SIGNAL_CMD, int *)
+#define HKD_SET_SIGNAL_WATCHER \
+       _IOW(HKD_MAJOR, HKD_SET_SIGNAL_WATCHER_CMD, pid_t *)
+
+#endif
